9706b75046441d0c19e37624a72240e5d4e693a9
codermahidul <codermahidul@gmail.com>
2024-04-02 01:54:36 +0600

    Breadcrumb issu solved

--- resources/views/components/dashboard/pages/faqsupdateform.blade.php
+++ resources/views/components/dashboard/pages/faqsupdateform.blade.php
@@ -6,7 +6,7 @@
           <!-- general form elements -->
           <div class="card card-primary">
             <div class="card-header">
-              <h3 class="card-title">Add New FAQs</h3>
+              <h3 class="card-title">Edit FAQs</h3>
             </div>
             <!-- /.card-header -->
             <!-- form start -->
